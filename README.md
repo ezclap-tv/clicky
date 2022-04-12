fa4b9562224bb3f6818e8229b45db854f3cf83dc
jprochazk <1665677+jprochazk@users.noreply.github.com>
2022-04-12 12:24:12 +0200

    add file::Error enum + remove memory mapping

--- README.md
+++ README.md
@@ -15,7 +15,11 @@ $ ./target/release/clicky
 ```
 
 ## Saving the count
-A persistent counter backend(s) may be selected and enabled at compile using cargo features. For example, in order to use the file backend, use the following command:  `cargo build --release --features=backend-file`
+A persistent counter backend(s) may be selected and enabled at compile time using cargo features. For example, in order to use the file backend, use the following command:
+
+```bash
+$ cargo build --release --features=backend-file
+```
 
 ### File backend
 Periodically saves the counter to a text file.
@@ -24,7 +28,7 @@ Periodically saves the counter to a text file.
 | --------------- |
 | `backend-file`  |
 
-| Env Variable              | Description                                                                                                                                                                                           | Default      |
-| :-----------------------: | ----------------------------------------------------------------------------                                                                                                                          | ---------    |
-| `CLICKY_COUNTER_FILE`     | Path to the file that should store the number of clicks.                                                                                                                                              | `clicky.txt` |
-| `CLICKY_SYNC_FREQUENCY`   | The frequency with which the counter should be written to the file. Must be a number between 1 and 65536, followed by `ms`, `m`, or `s`. Providing an empty string or `0` will be treated as default. | `1s`         |
+| Env Variable | Description | Default |
+| :----------: | -------- | ------- |
+| `CLICKY_COUNTER_FILE` | Path to the file that should store the number of clicks. | `clicky.txt` |
+| `CLICKY_SYNC_FREQUENCY` | See [humantime::parse_duration](https://docs.rs/humantime/latest/humantime/fn.parse_duration.html) for supported formats | `1s` |
--- src/storage/file.rs
+++ src/storage/file.rs
@@ -1,3 +1,5 @@
+use std::io::Cursor;
+
 use {
   crate::Count,
   actix_web::web,
@@ -8,6 +10,18 @@ use {
   },
 };
 
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+  #[error("{0}")]
+  Io(#[from] std::io::Error),
+  #[error("{0}")]
+  ParseInt(#[from] std::num::ParseIntError),
+  #[error("{0}")]
+  Utf8(#[from] std::str::Utf8Error),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
 /// Memory mapped file storage
 pub struct FileStorage {
   file: File,
@@ -16,9 +30,32 @@ pub struct FileStorage {
 
 const MIN_DURATION: Duration = Duration::new(1, 0);
 
+fn parse_file_contents(file: &mut File) -> Result<u64> {
+  use std::io::{Seek, SeekFrom};
+  file.seek(SeekFrom::Start(0))?;
+  let mut buf = [0; 20];
+  let len = file.read(&mut buf)?;
+  let str = std::str::from_utf8(&buf[..len])?;
+  if str.is_empty() {
+    Ok(0)
+  } else {
+    Ok(str.parse()?)
+  }
+}
+
+fn write_file_contents(file: &mut File, value: u64) -> Result<()> {
+  use std::io::{Seek, SeekFrom, Write};
+  file.seek(SeekFrom::Start(0))?;
+  let mut buf = Cursor::new([0u8; 20]);
+  write!(&mut buf, "{value}")?;
+  let (pos, buf) = (buf.position() as usize, buf.into_inner());
+  file.write_all(&buf[..pos])?;
+  Ok(())
+}
+
 impl FileStorage {
-  pub fn from_env() -> std::io::Result<Self> {
-    let path = std::env::var("CLICKY_COUNTER_FILE").unwrap_or_else(|_| "clicky.bin".into());
+  pub fn from_env() -> Result<Self> {
+    let path = std::env::var("CLICKY_COUNTER_FILE").unwrap_or_else(|_| "clicky.txt".into());
     let sync_frequency = match std::env::var("CLICKY_SYNC_FREQUENCY").ok() {
       Some(v) => humantime::parse_duration(&v)
         .map(|v| {
@@ -48,26 +85,20 @@ impl FileStorage {
     })
   }
 
-  pub fn install(self, counter: web::Data<Count>) -> std::io::Result<()> {
+  pub fn install(self, counter: web::Data<Count>) -> Result<()> {
     let Self {
       mut file,
       sync_frequency,
     } = self;
 
-    let mut buf = [0; 8];
-    let _ = file.read(&mut buf)?;
-    file.set_len(8)?;
-    counter.set(u64::from_le_bytes(buf));
+    counter.set(parse_file_contents(&mut file)?);
 
-    let mut mmap = unsafe { memmap2::MmapMut::map_mut(&file) }?;
     std::thread::spawn(move || {
       let mut previous_count = u64::MAX;
       loop {
-        use std::io::Write;
-
         let count = counter.get();
         if count != previous_count {
-          if let Err(e) = (&mut mmap[..]).write_all(&count.to_le_bytes()) {
+          if let Err(e) = write_file_contents(&mut file, count) {
             panic!("Failed to persist the counter at {count}: {e}");
           } else {
             log::info!("Persisted counter at {count}");
